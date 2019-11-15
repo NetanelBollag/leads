a05e5d92005f735991cd4248f9d5b946b9258cdc
Netanel Bollag <nbollag@gmail.com>
2019-11-15 21:40:27 +0200

    Add first action - GET_LEADS
    Signed-off-by: Netanel Bollag <nbollag@gmail.com>

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,13 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropType from 'prop-types';
+import { getLeads } from '../../actions/leads';
 
 export class Leads extends Component {
+    static PropType = {
+        leads: PropType.array.isRequired
+    }
+
+    componentDidMount() {
+        this.props.getLeads();
+    }
+
     render() {
         return (
-            <div>
-                Leads list
-            </div>
+            <Fragment>
+                <h2>Leads</h2>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Message</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.props.leads.map(lead => (
+                                <tr key={lead.id}>
+                                    <td>{lead.id}</td>
+                                    <td>{lead.name}</td>
+                                    <td>{lead.email}</td>
+                                    <td>{lead.message}</td>
+                                    <td><button className="btn btn-danger btn-sm">Delete</button></td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </Fragment>
         )
     }
 }
 
-export default Leads
+const mapStateToProps = state => ({
+    leads: state.leads.leads
+})
+
+export default connect(mapStateToProps, { getLeads })(Leads);
