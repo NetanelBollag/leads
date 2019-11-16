3708561c87330020a0c8f51f100c1f64e31856ae
Netanel Bollag <nbollag@gmail.com>
2019-11-16 22:30:36 +0200

    Add authentication request for lead getting
    Signed-off-by: Netanel Bollag <nbollag@gmail.com>

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { createMessage } from './messages';
+import { createMessage, returnErrors } from './messages';
 
 import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types';
 
@@ -10,7 +10,10 @@ export const getLeads = () => dispatch => {
                 type: GET_LEADS,
                 payload: res.data
             });
-        }).catch(err => console.log(err));
+        }).catch(err =>
+            dispatch(returnErrors(err.response.data,
+                err.response.status))
+        );
 }
 
 export const deleteLead = (id) => dispatch => {
@@ -23,27 +26,19 @@ export const deleteLead = (id) => dispatch => {
                 type: DELETE_LEAD,
                 payload: id
             });
-        }).catch(err => console.log(err));
+        }).catch(err => dispatch(returnErrors
+            (err.response.data, err.response.status)));
 }
 
 export const addLead = (lead) => dispatch => {
-    axios.post('/api/leads/', lead)
-        .then(res => {
-            dispatch(createMessage({
-                addLead: "Lead added"
-            }));
-            dispatch({
-                type: ADD_LEAD,
-                payload: res.data
-            });
-        }).catch(err => {
-            const errors = {
-                msg: err.response.data,
-                status: err.response.status
-            };
-            dispatch({
-                type: GET_ERRORS,
-                payload: errors
-            });
+    axios.post('/api/leads/', lead).then(res => {
+        dispatch(createMessage({
+            addLead: "Lead added"
+        }));
+        dispatch({
+            type: ADD_LEAD,
+            payload: res.data
         });
+    }).catch(err => dispatch(returnErrors
+        (err.response.data, err.response.status)));
 };
