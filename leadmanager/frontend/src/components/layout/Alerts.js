da4b1b45e904213859ea4ac201155bba41d88126
Netanel Bollag <nbollag@gmail.com>
2019-11-23 13:04:38 +0200

    Welcome msg
    Signed-off-by: Netanel Bollag <nbollag@gmail.com>

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -18,6 +18,8 @@ export class Alerts extends Component {
                 alert.error(`Email: ${error.msg.email.join()}`);
             if (error.msg.message)
                 alert.error(`Message: ${error.msg.message.join()}`);
+            if (error.msg.non_field_errors)
+                alert.error(error.msg.non_field_errors.join());
         }
 
         if (message !== prevProps.message) {
